bcc7a1ca086176f7a4a7876a17726234e35f5fb4
u31d74 <u31d74@INGURWN110444>
2023-11-03 10:25:43 +0530

    Add project files.

---
+++ Assessment/AssessmentProgram.cs
@@ -0,0 +1,43 @@
+﻿using Assessment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+    class AssessmentProgram
+    {
+        static void Main()
+        {
+            List<Pizza> pizzaOrders = new List<Pizza>();
+            Pizza pizza1 = new Pizza("Medium", 1, 2, 0);
+            Pizza pizza2 = new Pizza("Large", 2, 1, 3);
+            Pizza pizza3 = new Pizza("Medium", 4, 1, 3);
+            Pizza pizza4 = new Pizza("Large", 2, 4, 3);
+            Pizza pizza5 = new Pizza("Small", 2, 1, 1);
+            pizzaOrders.Add(pizza1);
+            pizzaOrders.Add(pizza2);
+            pizzaOrders.Add(pizza3);
+            pizzaOrders.Add(pizza4);
+            pizzaOrders.Add(pizza5);
+
+            /*
+            int Number;
+            Number = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter number of Pizzas");
+            for(int i = 0;i< Number; i++) {
+                pizzaOrders.Add(pizzaOrders[i]);
+            }
+            */
+
+            foreach (Pizza pizza in pizzaOrders)
+            {
+                Console.WriteLine(pizza.GetDescription());
+                Console.WriteLine("TotalCost: $" + pizza.CalcCost());
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Assessment/Pizza.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+    internal class Pizza
+    {
+        private string size;
+        private int cheeseToppings;
+        private int pepperoniToppings;
+        private int hamToppings;
+
+        public string Size
+        {
+            get 
+            { 
+                return size;
+            }
+            set 
+            { 
+                size = value;
+            }
+        }
+
+        public int CheeseToppings
+        {
+            get
+            { 
+                return cheeseToppings; 
+            }
+            set 
+            { 
+                cheeseToppings = value;
+            }
+        }
+
+        public int PepperoniToppings
+        {
+            get 
+            { 
+                return pepperoniToppings;
+            }
+            set
+            { 
+                pepperoniToppings = value;
+            }
+        }
+
+        public int HamToppings
+        {
+            get
+            { 
+                return hamToppings;
+            }
+            set
+            { 
+                hamToppings = value;
+            }
+        }
+
+        public Pizza(string size, int cheeseToppings, int pepperoniToppings, int hamToppings)
+        {
+            this.size = size;
+            this.cheeseToppings = cheeseToppings;
+            this.pepperoniToppings = pepperoniToppings;
+            this.hamToppings = hamToppings;
+        }
+
+        public double CalcCost()
+        {
+            double cost = 0;
+
+            switch (size)
+            {
+                case "Small":
+                    cost = 10 + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+                    break;
+                case "Medium":
+                    cost = 12 + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+                    break;
+                case "Large":
+                    cost = 14 + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+                    break;
+                default:
+                    Console.WriteLine("INVALID SIZE");
+                    break;
+            }
+
+            return cost;
+        }
+
+        public string GetDescription()
+        {
+            return $"{size} Pizza with {cheeseToppings} Cheese Toppings, {pepperoniToppings} Pepperoni Toppings, and {hamToppings} Ham Toppings.";
+        }
+    }
+}
